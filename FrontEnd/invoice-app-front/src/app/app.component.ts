3e1aa80863508c6654934ce8b2a33b57f89ccee3
Edwin HC <edwinrhc@gmail.com>
2024-06-08 12:23:04 -0500

    Creando proyecto InvoiceApp Front

--- FrontEnd/invoice-app-front/src/app/app.component.ts
+++ FrontEnd/invoice-app-front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {InvoiceComponent} from "./components/invoice/invoice.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [InvoiceComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ FrontEnd/invoice-app-front/src/app/components/invoice/invoice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice.component.html'
+})
+export class InvoiceComponent {
+
+}
