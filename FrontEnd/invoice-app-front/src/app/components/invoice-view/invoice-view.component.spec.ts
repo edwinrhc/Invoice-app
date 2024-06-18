666634f7115ac7d4aa9cf10be5f5637cc51c82f4
Edwin HC <edwinrhc@gmail.com>
2024-06-18 10:07:14 -0500

    Create Component Invoice-View

---
+++ FrontEnd/invoice-app-front/src/app/components/invoice-view/invoice-view.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InvoiceViewComponent } from './invoice-view.component';
+
+describe('InvoiceViewComponent', () => {
+  let component: InvoiceViewComponent;
+  let fixture: ComponentFixture<InvoiceViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [InvoiceViewComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(InvoiceViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ FrontEnd/invoice-app-front/src/app/components/invoice-view/invoice-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice-view.component.html',
+  styleUrl: './invoice-view.component.css'
+})
+export class InvoiceViewComponent {
+
+}
